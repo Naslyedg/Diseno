1c34daed4e7d7197dfbdd0086709981503c19350
Naslyedg <nasdiazg@hotmail.com>
2017-04-16 00:11:22 -0500

    Commit2
    Link

--- js/myscripts.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-  alert('sadfsadf');
-  $('a').click(function () {
-      var url = $(this).attr('rel');
-      console.log(url);
-      $('#iframe').attr('src', url);
-      $('#iframe').reload();
-  });
-});
